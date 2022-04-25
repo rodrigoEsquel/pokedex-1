a1ecfc246111b923c612f87a15885aee441afc3a
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-04-24 23:19:37 -0300

    wip test jest

--- src/ui/__tests__/pokemon.specs.js
+++ src/ui/__tests__/pokemon.specs.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="Jest" />
 
-import mostrarPokemon from './ui/pokemon.js';
-import pokemonFixture from '../../cypress/fixtures/bulbasaur.json';
-
+import mostrarPokemon from '../pokemon.js';
+import fixture from '../../__tests__/pokedex.fixture.js';
+import pokemonFixture from '../../../cypress/fixtures/bulbasaur.json';
+import mapearPokemon from '../../mapeadores/pokemon.js';
+import cargarPokemon from '../../servicios/pokemon.js';
 
 describe('pokemon', () => {
-  it('deberia obtener los parametros de la url', () => {
+  it('deberia actualizar un pokemon cargado', async () => {
+    document.body.innerHTML = fixture;
     global.fetch = jest.fn()
       .mockImplementation(() => new Promise((resolve) => {
         const jsonPromise = new Promise((r) => {
@@ -14,6 +17,15 @@ describe('pokemon', () => {
         });
         resolve({ json: () => jsonPromise });
       }));
-    mostrarPokemon(pokemonFixture);
+    mostrarPokemon({
+      id: 1,
+      nombre: 'hola',
+      foto: 'foto',
+      tipos: ['roca'],
+      habilidades: ['hola', 'chau'],
+      movimientos: ['hola', 'chau'],
+    });
+
+    expect(document.querySelector(('#ayuda')).textContent).toBe('Seleccioná un pokemon para ver su información');
   });
 });
